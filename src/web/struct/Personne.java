2cd88ddf2b0c14e395c25900ae24125d6f47d4ad
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2016-12-16 09:41:00 +0100

    Salut

---
+++ src/web/struct/Personne.java
@@ -0,0 +1,67 @@
+package web.struct;
+
+import java.util.GregorianCalendar;
+
+public class Personne {
+	private String login;
+	private String prenom;
+	private String nom;
+	private String mail;
+	private String tel;
+	private GregorianCalendar dateNaiss;
+	private String role;
+	
+	public Personne(String login, String prenom, String nom, String mail, String tel, GregorianCalendar dateNaiss, String role) {
+		super();
+		this.login = login;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.mail = mail;
+		this.tel = tel;
+		this.dateNaiss = dateNaiss;
+		this.role = role;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public GregorianCalendar getDateNaiss() {
+		return dateNaiss;
+	}
+	public void setDateNaiss(GregorianCalendar dateNaiss) {
+		this.dateNaiss = dateNaiss;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
