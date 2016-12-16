94e3c95bb599fe046cd4576cd11d3de5d9fd0680
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2016-12-16 11:17:13 +0100

    truc

--- comp.sh
+++ comp.sh
@@ -1,1 +1,2 @@
-javac -sourcepath src -cp lib/ -d WEB-INF/classes/ src/web/struct/*
+javac -sourcepath src -cp lib/servlet-api.jar:lib/postgresql-9.3-1102.jdbc41.jar -d WEB-INF/classes/ src/web/servlet/*
+javac -sourcepath src -cp lib/servlet-api.jar:lib/postgresql-9.3-1102.jdbc41.jar -d WEB-INF/classes/ src/web/struct/*
