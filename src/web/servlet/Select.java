f0c64b05d3250a5a22162b044bcc9dec1cfed50a
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-01-06 16:50:49 +0100

    git push

--- src/web/servlet/Select.java
+++ src/web/servlet/Select.java
@@ -29,69 +29,73 @@ public class Select extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		PrintWriter out = res.getWriter();
-		HttpSession session = req.getSession();
-		
-		if (((Personne)session.getAttribute("personne")) == null) {
-			System.out.println("ohfdvk:ugesdlihvdsm!xiwfhgvlisdwfligvol:wqsegrfvoligsewo:lgr:ol");
-		}
-		
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet rsAbs = null;
-		ResultSet rsJus = null;
-		
-		String sql = "SELECT p.prenom,p.nom,a.date,a.demiJ FROM personne as p inner join absences as a on a.login = p.login WHERE p.role = 'etu' and p.login='"
-				+ ((Personne) session.getAttribute("personne")).getLogin() + "';";
-		
-		String sqlJus = "SELECT date from justif where login='"
-				+ ((Personne) session.getAttribute("personne")).getLogin() + "';";
-
-		ArrayList<String> justifs = new ArrayList<String>();
-
-		try {
-			Class.forName("org.postgresql.Driver");
-			con = DriverManager.getConnection(URL, NOM, MDP);
-			stmt = con.createStatement();
-			rsJus = stmt.executeQuery(sqlJus);
-			while (rsJus.next()) {
-				justifs.add(rsJus.getString(1));
+		HttpSession session = req.getSession(true);
+		if (((Personne) session.getAttribute("personne")) == null) {
+			res.sendRedirect("../login.html");
+		} else {
+			if (((Personne) session.getAttribute("personne")) == null) {
+				System.out.println("drpfogijspçddsfgstre0000000000000000erjhgçpêsh)çtrh");
+				res.sendRedirect("../menu.jsp");
 			}
-			rsAbs = stmt.executeQuery(sql);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 
-		out.println("<!DOCTYPE html><html lang='fr'>"
-				+ "<head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1'>"
-				+ "<title>Vos Absences</title>"
-				+ "<!-- Bootstrap core CSS --><link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>"
-				+ "</head>");
-		out.println("<body>");
-		out.println("<center><table class='table table-bordered'>");
-		try {
-			out.println("<th>prenom</th><th>nom</th><th>date</th><th>horaire</th><th>Justifie</th>");
-			while (rsAbs.next()) {
-				if (justifs.contains(rsAbs.getString(3)))
-					out.println("<tr><td>" + rsAbs.getString(1) + "</td><td>" + rsAbs.getString(2) + "</td><td>"
-							+ rsAbs.getString(3) + "</td><td>" + rsAbs.getString(4) + "</td><td>oui</td></tr>");
-				else
-					out.println("<tr><td>" + rsAbs.getString(1) + "</td><td>" + rsAbs.getString(2) + "</td><td>"
-							+ rsAbs.getString(3) + "</td><td>" + rsAbs.getString(4) + "</td><td>non</td></tr>");
+			Connection con = null;
+			Statement stmt = null;
+			ResultSet rsAbs = null;
+			ResultSet rsJus = null;
 
+			String sql = "SELECT p.prenom,p.nom,a.date,a.demiJ FROM personne as p inner join absences as a on a.login = p.login WHERE p.role = 'etu' and p.login='"
+					+ ((Personne) session.getAttribute("personne")).getLogin() + "';";
+
+			String sqlJus = "SELECT date from justif where login='"
+					+ ((Personne) session.getAttribute("personne")).getLogin() + "';";
+
+			ArrayList<String> justifs = new ArrayList<String>();
+
+			try {
+				Class.forName("org.postgresql.Driver");
+				con = DriverManager.getConnection(URL, NOM, MDP);
+				stmt = con.createStatement();
+				rsJus = stmt.executeQuery(sqlJus);
+				while (rsJus.next()) {
+					justifs.add(rsJus.getString(1));
+				}
+				rsAbs = stmt.executeQuery(sql);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		out.println("</table><center>");
-		out.println("</body>");
-		out.println("</html>");
 
-		try {
-			con.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+			out.println("<!DOCTYPE html><html lang='fr'>"
+					+ "<head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1'>"
+					+ "<title>Vos Absences</title>"
+					+ "<!-- Bootstrap core CSS --><link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>"
+					+ "</head>");
+			out.println("<body>");
+			out.println("<center><table class='table table-bordered'>");
+			try {
+				out.println("<th>prenom</th><th>nom</th><th>date</th><th>horaire</th><th>Justifie</th>");
+				while (rsAbs.next()) {
+					if (justifs.contains(rsAbs.getString(3)))
+						out.println("<tr><td>" + rsAbs.getString(1) + "</td><td>" + rsAbs.getString(2) + "</td><td>"
+								+ rsAbs.getString(3) + "</td><td>" + rsAbs.getString(4) + "</td><td>oui</td></tr>");
+					else
+						out.println("<tr><td>" + rsAbs.getString(1) + "</td><td>" + rsAbs.getString(2) + "</td><td>"
+								+ rsAbs.getString(3) + "</td><td>" + rsAbs.getString(4) + "</td><td>non</td></tr>");
+
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			out.println("</table><center>");
+			out.println("</body>");
+			out.println("</html>");
+
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
